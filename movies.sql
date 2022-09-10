c82e6e5f535a265bd168ee5bf46d9c4d64304dd7
Hoodoo42 <create@hoodooverse.ca>
2022-09-10 15:16:50 -0600

    added a unique key for the combo of title, director, release date

--- movies.sql
+++ movies.sql
@@ -28,7 +28,8 @@ CREATE TABLE `movie` (
   `runtime` varchar(6) COLLATE utf8mb4_bin DEFAULT NULL,
   `director` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
   `release_year` varchar(65) COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `movie_UN` (`title`,`director`,`release_year`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -58,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-10 14:53:48
+-- Dump completed on 2022-09-10 15:15:48
